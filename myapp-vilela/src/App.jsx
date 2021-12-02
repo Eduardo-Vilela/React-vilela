6c7aeb0d58ce2fbcc561fb3569e6a8f6095c6e19
Eduardo-Vilela <edumix22@gmail.com>
2021-12-02 16:54:04 -0300

    adding navbar

---
+++ myapp-vilela/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar/NavBar.jsx';
+function App() {
+  return (
+    <div >
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
--- myapp-vilela/src/components/NavBar/NavBar.jsx
+++ myapp-vilela/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-function NavBar() {
-    return (
-        <div>
+import "./style.css";
 
-        </div>
-    )
-}
+const NavBar = () => {
+  return (
+        <header className="navbar">
+            <div className="navbartitle navbaritem">icono/marca</div>
+            <div className="navbaritem">Productos</div>
+            <div className="navbaritem">Contacto</div>
+            <div className="navbar__item">Ayuda</div>
+        </header>
+  );
+};
 
-export default NavBar
+export default NavBar;
