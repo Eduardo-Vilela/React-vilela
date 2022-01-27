688e27691039581e675b0c091fad9a80f7202a4f
Eduardo-Vilela <edumix22@gmail.com>
2022-01-27 17:04:08 -0300

    finalizando funcionamiento

--- myapp-vilela/src/components/demo/Demo.js
+++ myapp-vilela/src/components/demo/Demo.js
@@ -1,33 +1,42 @@
-import React, { useContext } from 'react'
-import { CartContext } from '../../Context/CartContext.jsx'
-import { Card, Button, Form} from "react-bootstrap"
+import React, { useContext, useState } from 'react'
+import { CartContext } from '../../Context/CartContext'
+import { Card, Button} from "react-bootstrap"
 import { BsFillTrashFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
-import { addDoc, collection , getFireStore } from 'firebase/firestore';
+import { addDoc, collection , getFirestore ,Timestamp } from 'firebase/firestore';
 
-function cart(){
-    const{ cartList,borrarItem,borrarCarrito,precioTotal} = useContext(CartContext)
-
-    const generaOrden = (e) => {
+function Cart(){
+    const{ cartList,borrarItem,borrarCarrito,precioTotal } = useContext(CartContext)
+    const [idOrden, setIdOrden] = useState("")
+    
+  
+    const generarOrden = (e) => {
       e.preventDefault()
+
       let orden = {}
-
-
+      orden.date = Timestamp.fromDate(new Date())
       orden.buyer ={ nombre: 'fede', tel:'1123644',email: 'edumix22@gmail.com'}
       orden.total = precioTotal();
-      orden.items = cartList.map(carItem => { 
+      orden.items = cartList.map(cartProducto => { 
         const id = cartProducto.id;
-        const nombre = cartProducto.name;
-        const precio = cartProducto.price =cartItem.cantidad;
+        const nombre = cartProducto.title;
+        const precio = cartProducto.price * cartProducto.cantidad;
+        const cantidad = cartProducto.cantidad;
 
-        return {id,nombre,precio }
+        return {id,nombre,precio,cantidad }
       })
-      const db = getFireStore()
-      const ordenColeccion = collection(db , 'orders')
-      addDoc(ordenColeccion, orden)
-      .then(resp => console.log(resp))
+      // const db = getFirestore()
+      // const ordenColleccion = collection(db , 'orders')
+      // addDoc(ordenColleccion, orden)
+      // .then(resp => console.log(resp))
       
-      // console.log(orden)
+      //  console.log(orden)
+      const db = getFirestore();
+        const ordenCollection = collection(db, 'Ordenes')
+        addDoc(ordenCollection, orden)
+        .then(resp => setIdOrden(resp.id))
+        .catch(err => console.log(err))
+        console.log(orden)
     }   
 
     return (
@@ -55,13 +64,11 @@ function cart(){
                <div>
                    <h1>Carrito Vacio</h1>
                    <Link to = "/">Seguir Comprando</Link>
-               <Form onSubmit={generaOrden}>
-                 
-               </Form>
                </div>
                }
+                 <Button onClick = {generarOrden}> Generar Orden</Button>
         </div>
     )
 }
 
-export default cart
+export default Cart
